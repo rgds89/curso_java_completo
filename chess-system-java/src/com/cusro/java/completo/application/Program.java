19c3ab1aa93e9b92b79ba2a1a3e5c568815da246
Roger Silva <rogersilvati@gmail.com>
2021-08-15 21:55:32 -0300

    Lesson 152 - Created piece class and board class

--- chess-system-java/src/com/cusro/java/completo/application/Program.java
+++ chess-system-java/src/com/cusro/java/completo/application/Program.java
@@ -1,10 +1,10 @@
 package com.cusro.java.completo.application;
 
+import com.cusro.java.completo.boardgame.Board;
 import com.cusro.java.completo.boardgame.Position;
 
 public class Program {
     public static void main(String[] args){
-        Position pos = new Position(2L, 4L);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
