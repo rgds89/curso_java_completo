9457f706a774f0aa74a0bb8a695d05f41be7cadd
Roger Silva <rogersilvati@gmail.com>
2021-09-20 11:22:58 -0300

    Lesson 156 - ChessException and ChessPosition

---
+++ chess-system-java/src/com/cusro/java/completo/chess/ChessException.java
@@ -0,0 +1,9 @@
+package com.cusro.java.completo.chess;
+
+public class ChessException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+}
