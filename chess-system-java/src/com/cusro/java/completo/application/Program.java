e44f7ecc906f39448394dacaf6efaa74f2b25dcd
Roger Silva <rogersilvati@gmail.com>
2021-09-20 15:09:22 -0300

    Lesson 159 - Handling exceptions and clearing screen

--- chess-system-java/src/com/cusro/java/completo/application/Program.java
+++ chess-system-java/src/com/cusro/java/completo/application/Program.java
@@ -1,27 +1,38 @@
 package com.cusro.java.completo.application;
 
+import com.cusro.java.completo.chess.ChessException;
 import com.cusro.java.completo.chess.ChessMatch;
 import com.cusro.java.completo.chess.ChessPiece;
 import com.cusro.java.completo.chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
-        while(true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(scanner);
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(scanner);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(scanner);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(scanner);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                scanner.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                scanner.nextLine();
+            }
         }
     }
 }
